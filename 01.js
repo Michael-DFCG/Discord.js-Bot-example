67dfa7138fc0bac74475b9ebcf36540d1743c547
NikiMichael <82709649+Michael-DFCG@users.noreply.github.com>
2021-07-06 08:49:07 +0200

    Update 01.js

--- 01.js
+++ 01.js
@@ -2,3 +2,9 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.login(process.env.token);
+
+// or 
+
+const token = require("./config.json")
+
+client.login(token)
